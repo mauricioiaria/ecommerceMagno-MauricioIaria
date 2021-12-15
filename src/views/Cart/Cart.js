f9f676f061b3ab767c7660bd39e0d71f2d7e5c2f
Mauricio Iaria <mauricioiaria@gmail.com>
2021-12-14 21:04:37 -0300

    Desafio Cartviews

--- src/views/Cart/Cart.js
+++ src/views/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import "../Cart/Cart.css";
 import { CartContext } from '../../components/Context/UseContext'
 
 const Cart = () => {
@@ -6,22 +7,54 @@ const Cart = () => {
 
     return (
         <>
-            <div>
-                {
-                    items.map((item) => (
+            <div className='container'>
+                {items.map((item) => (
+                    <div key={item.char_id}>
+                        <br></br>
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr className='text-primary'>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Producto</th>
+                                    <th scope="col">Categoria</th>
+                                    <th scope="col">Cantidad</th>
+                                    <th scope="col">Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">{item.char_id}</th>
+                                    <td className='table__productos'>{item.name}</td>
+                                    <td className='table__category'> {item.nickname}</td>
+                                    <td className='table__cantidad'>{item.count}</td>
+                                    <td className='table__price'>$ {item.nickname}</td>
 
-                        <div key={item.char_id}>
-                            <h1>{item.name}</h1>
-                            <h2>{item.nickname}</h2>
-                            <h3>{item.birthday}</h3>
-                            <h4 onClick={() => removeItem(item.char_id)}>BORRAR PRODUCTO</h4>
-                        </div>
-                    ))
-                }
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button onClick={() => removeItem(item.char_id)} type="button" class="btn btn-warning">ELIMINAR PRODUCTO</button>
+                    </div>))}
             </div>
-            <h4 onClick={(item) => clearItems(item.char_id)}>VACIAR CARRITO</h4>
+            <div className='container'>
+                <button onClick={(item) => clearItems(item.char_id)} type="button" class="btn-lg btn-danger mt-5">VACIAR CARRITO</button>
+            </div>
         </>
     )
 }
 
+
+/* <Card>
+    <Image src={item.img} wrapped ui={false} />
+    <Card.Content>
+        <Card.Header>{item.name}</Card.Header>
+        <Card.Meta>
+            <span className='date'>Categoria: {item.birthday}</span>
+        </Card.Meta>
+        <Card.Description>
+            $ {item.nickname}
+        </Card.Description>
+    </Card.Content>
+</Card> */
+
 export default Cart
+
