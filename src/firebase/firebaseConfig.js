e88b2db9b6f51125793aa6be2f247165b4aedf88
Mauricio Iaria <mauricioiaria@gmail.com>
2022-01-05 18:40:11 -0300

    proyecto Final Ecommerce-magno(resubido)

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,21 +3,21 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
 
-    // apiKey: "AIzaSyAIyc_mDy4fc7SghKGgUEoOTBf0wGIv8go",
-    // authDomain: "magno-ecommerce.firebaseapp.com",
-    // projectId: "magno-ecommerce",
-    // storageBucket: "magno-ecommerce.appspot.com",
-    // messagingSenderId: "660728166631",
-    // appId: "1:660728166631:web:a4e0fd0a943cfed1e68a42"
+    apiKey: 'AIzaSyAIyc_mDy4fc7SghKGgUEoOTBf0wGIv8go',
+    authDomain: 'magno-ecommerce.firebaseapp.com',
+    projectId: 'magno-ecommerce',
+    storageBucket: 'magno-ecommerce.appspot.com',
+    messagingSenderId: '660728166631',
+    appId: '1:660728166631:web:a4e0fd0a943cfed1e68a42',
 
     // Reemplazo de datos por variables de entorno
 
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
+    // apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    // appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
